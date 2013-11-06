dbd0085a73d3924000bf6007b9d1855b55a6e7b8
Nallasamy Ponnusamy <ponnusamy@fas.harvard.edu>
2013-11-06 16:47:55 -0500

    Patch 9.91

--- views/v_users_signupSuccess.php
+++ views/v_users_signupSuccess.php
@@ -1,5 +1,5 @@
 
-<<!-- This page is under construction, just redirect the user-->
+<!-- This page is under construction, just redirect the user-->
     <h1>Congratulations!, You have Successfully Singed up!</h1>
 	<BR>
     <a href='/users/login'>CLICK here to Login!</a>
