85409ac5fa5f7ab0a2a20197ca7991c43ff97320
Nallasamy Ponnusamy <ponnusamy@fas.harvard.edu>
2013-11-06 16:11:46 -0500

    Patch 9.5 - Final Touch Up

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,17 +1,19 @@
+<?php $int = 0; ?>
 <?php foreach($users as $user): ?>
-
+    <?php $int++; ?>
+    <li class="<?php echo fmod($int, 2) ? 'even' : 'odd' ?>"
     <!-- Print this user's name -->
     <?=$user['first_name']?> <?=$user['last_name']?>
 
     <!-- If there exists a connection with this user, show a unfollow link -->
     <?php if(isset($connections[$user['user_id']])): ?>
-        <a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+        <a class='unfollow' href='/posts/unfollow/<?=$user['user_id']?>'><br>Unfollow</a>
 
     <!-- Otherwise, show the follow link -->
     <?php else: ?>
-        <a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+        <a class='follow' href='/posts/follow/<?=$user['user_id']?>'><br>Follow</a>
     <?php endif; ?>
 
     <br><br>
-
+    </li>
 <?php endforeach; ?>
